cba37d2ec57efd1146ceba45aad575e5122fae97
PNP4Help <31499171+pnp4help@users.noreply.github.com>
2019-01-11 09:50:11 +0530

    Icon
    Icon

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/cryptocalsi/it/cspit/charusat/crypto/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/cryptocalsi/it/cspit/charusat/crypto/R.java
@@ -4002,7 +4002,7 @@ public final class R {
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f0c0000;
-    public static final int ic_launcher_round=0x7f0c0001;
+    public static final int ic_launcher_foreground=0x7f0c0001;
   }
   public static final class string {
     public static final int abc_action_bar_home_description=0x7f0d0000;
